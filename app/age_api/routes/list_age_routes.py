0c0433d82fb6282d4144b81891e355306e68539f
ahdev <53409839+ahslcdev@users.noreply.github.com>
2024-04-29 10:59:45 -0300

    style: correção na estrutura do codigo

--- app/age_api/routes/list_age_routes.py
+++ app/age_api/routes/list_age_routes.py
@@ -7,16 +7,18 @@ from app.age_api.routes import router
 from app.age_api.schemas import AgeGroup
 from app.age_api.service import AgeGroupService
 
-@router.get("/",
-            response_model=List[AgeGroup],
-            responses={
-                status.HTTP_400_BAD_REQUEST:{
-                    "model":ErrorMessageBadRequest
-                },
-                status.HTTP_401_UNAUTHORIZED:{
-                    "model":ErrorMessageAgeGroupNotAuth
-                }
-            })
+@router.get(
+    "/",
+    response_model=List[AgeGroup],
+    responses={
+        status.HTTP_400_BAD_REQUEST:{
+            "model":ErrorMessageBadRequest
+        },
+        status.HTTP_401_UNAUTHORIZED:{
+            "model":ErrorMessageAgeGroupNotAuth
+        }
+    }
+)
 async def list_age_groups(Verifcation = Depends(ConfigAuth.check_credentials)):
     try:
         ages = AgeGroupService().get_instances()
