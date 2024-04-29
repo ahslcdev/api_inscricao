6ba60a82226f9546d8995ef3959237914e729b76
ahdev <53409839+ahslcdev@users.noreply.github.com>
2024-04-29 13:49:41 -0300

    refactor: ajustando as validações no schema

--- app/enrollment_api/schemas.py
+++ app/enrollment_api/schemas.py
@@ -1,27 +1,29 @@
 from pydantic import BaseModel, validator
 
 from app.age_api.service import AgeGroupService
-from app.enrollment_api.exceptions import InvalidCPF
+from app.enrollment_api.exceptions import InvalidAge, InvalidCPF
 from app.enrollment_api.utils import validate_cpf
 
 class EnrollmentSchema(BaseModel):
     name: str
-    age: str = None
     cpf: str
     age: int
 
     @validator('cpf')
     def valid_cpf(cls, value):
         if not validate_cpf(value):
-            raise InvalidCPF("CPF must have 11 digits")
+            raise InvalidCPF("CPF inválido")
         return value
     
     @validator('age')
     def valid_age(cls, value):
+        if value < 0:
+            raise InvalidAge("Idade não pode ser menor que 0")
         instange_service =  AgeGroupService()
         query = instange_service.get_query()
         values = (query.min_age <= value) & (query.max_age >= value)
         ages = instange_service.get_by_query(values)
         if not ages:
-            raise InvalidCPF("Invalido")
+            raise InvalidAge("Esta idade não está disponível.")
+        
         return value
