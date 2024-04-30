c1b578e5de0b3eda8d93ed2b8f757747b73e2cb4
ahdev <53409839+ahslcdev@users.noreply.github.com>
2024-04-30 11:54:02 -0300

    refactor: mudando os validators de arquivo

--- app/enrollment_api/utils.py
+++ app/enrollment_api/utils.py
@@ -1,3 +1,7 @@
+from app.age_api.service import AgeGroupService
+from app.enrollment_api.exceptions import InvalidAge
+
+
 def validate_cpf(cpf):
     # Remove alguns caracteres indesejados
     cpf = ''.join([c for c in cpf if c.isdigit()])
@@ -18,3 +22,16 @@ def validate_cpf(cpf):
             return False
 
     return True
+
+
+def validate_age(value):
+    if value <= 0:
+        raise InvalidAge("Idade não pode ser menor que 0")
+    instange_service =  AgeGroupService()
+    query = instange_service.get_query()
+    values = (query.min_age <= value) & (query.max_age >= value)
+    ages = instange_service.get_by_query(values)
+    if not ages:
+        raise InvalidAge("Esta idade não está disponível.")
+    
+    return value
