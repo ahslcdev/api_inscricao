db4e74e95666d0278a5668d5c2f9efda0c8f3b1b
ahdev <53409839+ahslcdev@users.noreply.github.com>
2024-04-30 11:53:49 -0300

    Update service.py

--- app/enrollment_api/service.py
+++ app/enrollment_api/service.py
@@ -1,6 +1,7 @@
 from app.database.tinydb import TinyDBInstance
 from celery.result import AsyncResult
 
+from app.enrollment_api.schemas import EnrollmentSchema
 from app.services.mixin_service import ServiceMixin
 
 class EnrollmentService(ServiceMixin):
@@ -14,7 +15,7 @@ class EnrollmentService(ServiceMixin):
     def get_query(self):
         return self.get_db().query
     
-    def create_instance(self, enrollment: dict):
+    def create_instance(self, enrollment: EnrollmentSchema):
         table = self.get_table()
         enrollment_id = table.insert(enrollment)
         return enrollment_id
@@ -22,3 +23,19 @@ class EnrollmentService(ServiceMixin):
     def get_status_enrollment(self, id: str):
         status: bool = AsyncResult(id)
         return status.ready()
+    
+    def delete_by_id(self, id: int):
+        ...
+        # return super().delete_by_id(id)
+    
+    def get_by_query(self, query):
+        ...
+        # return super().get_by_query(query)
+
+    def get_instances(self):
+        ...
+        # return super().get_instances()
+
+    def get_by_id(self, id: int):
+        ...
+        # return super().get_by_id(id)
